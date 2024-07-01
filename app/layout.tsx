86e358e6fa2d6745a52912ec40a4992a0e8c395b
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-01 23:07:47 +0200

    dark and light mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 
 
+
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200" ,"400", "500", "600", "700", "800", "900"],});
 
 export const metadata: Metadata = {
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <>
     <html lang="en">
       <body className={cn(poppins.className, "dark")}>{children}</body>
     </html>
+    </>
   );
 }
