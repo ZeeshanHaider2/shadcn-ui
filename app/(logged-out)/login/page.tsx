43e49ff96ca21febb0affa5d2b4bf93b76990579
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-02 15:39:22 +0200

    login form created

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -4,7 +4,27 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { PersonStandingIcon } from "lucide-react";
+import * as z from 'zod';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password:z.string()
+})
 export default function login(){
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues:{
+            email: "",
+            password:""
+        }
+});
+const handleSubmit = () =>{
+    console.log("kkk")
+}
     return(
     <>
         <PersonStandingIcon size={50}/>
@@ -18,7 +38,43 @@ export default function login(){
                 </CardDescription>
             </CardHeader>
             <CardContent>
-                Login form
+                <Form {...form}>
+                    <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
+                    <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@xyz.com" type="email" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your email address for signing in .
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="password" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+            LOGIN
+        </Button>
+
+                    </form>
+                </Form>
             </CardContent>
             <CardFooter className="justify-between">
                 <small>Don't have an account?</small>
