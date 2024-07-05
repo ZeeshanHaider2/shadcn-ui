4dc5a86c6b309f432ff11c84656fbd91b1546088
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-05 17:54:13 +0200

    menu title created

---
+++ app/dashboard/components/main-menu.tsx
@@ -0,0 +1,7 @@
+import MenuTitle from "./menu-title";
+
+export default function MainMenu(){
+    return  <div className="bg-muted overflow-auto p-4">
+    <div className="border-b dark:border-b-black border-b-zink-300 pb-4"><MenuTitle/></div>
+    </div>
+}
