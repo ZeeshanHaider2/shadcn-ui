140dc1493e66f15401a1d92e1c669712f7adf99f
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-05 19:47:19 +0200

    side panel menu items added

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,7 +1,27 @@
+import MenuItem from "./menu-item";
 import MenuTitle from "./menu-title";
 
 export default function MainMenu(){
     return  <div className="bg-muted overflow-auto p-4">
-    <div className="border-b dark:border-b-black border-b-zink-300 pb-4"><MenuTitle/></div>
+    <div className="border-b dark:border-b-black border-b-zink-300 pb-4">
+        <MenuTitle/>
+    </div>
+    <div className="py-4">
+        <MenuItem href="/dashboard">
+            My dashboard
+        </MenuItem>
+        <MenuItem href="/dashboard/teams">
+          Teams
+        </MenuItem>
+        <MenuItem href="/dashboard/employees">
+            Employees
+        </MenuItem>
+        <MenuItem href="/dashboard/account">
+            Account
+        </MenuItem>
+        <MenuItem href="/dashboard/settings">
+           Settings
+        </MenuItem>
+    </div>
     </div>
 }
