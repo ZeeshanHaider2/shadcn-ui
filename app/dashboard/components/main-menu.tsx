6ebb6c5ba3cbd4a967cb52b4d0a25bcd545ad4b8
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-05 23:24:11 +0200

    sidebar footer with avatar added

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,12 +1,15 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import MenuItem from "./menu-item";
 import MenuTitle from "./menu-title";
+import Link from "next/link";
+import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
 
 export default function MainMenu(){
-    return  <div className="bg-muted overflow-auto p-4">
+    return  <div className="bg-muted overflow-auto p-4 flex flex-col">
     <div className="border-b dark:border-b-black border-b-zink-300 pb-4">
         <MenuTitle/>
     </div>
-    <div className="py-4">
+    <div className="py-4 grow">
         <MenuItem href="/dashboard">
             My dashboard
         </MenuItem>
@@ -22,6 +25,15 @@ export default function MainMenu(){
         <MenuItem href="/dashboard/settings">
            Settings
         </MenuItem>
+    </div>
+    <div className="flex gap-2 items-center">
+       <Avatar>
+        <AvatarFallback className="bg-blue-300 dark:bg-blue-600">
+            Zee
+        </AvatarFallback>
+       </Avatar>
+       <Link href="/" className="hover:underline">Logout</Link>
+       <LightDarkToggle className="ml-auto"/>
     </div>
     </div>
 }
