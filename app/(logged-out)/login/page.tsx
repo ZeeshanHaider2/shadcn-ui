d3d7bc82e6314d33f1f8c69991ae8c6586a8f0b4
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-10 22:57:25 +0200

    fixes

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -14,83 +14,75 @@ import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     email: z.string().email(),
-    password:z.string()
-})
-export default function login(){
-  const router = useRouter();
+    password: z.string()
+});
+
+export default function Login() {
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-        defaultValues:{
+        defaultValues: {
             email: "",
-            password:""
+            password: ""
         }
-});
-const handleSubmit = (data: z.infer<typeof formSchema>) =>{
-    console.log("kkk")
-    router.push("/dashboard")
-}
-    return(
-    <>
-        <PersonStandingIcon size={50}/>
-        <Card className="w-full max-w-sm">
-            <CardHeader>
-                <CardTitle>
-                    Login
-                </CardTitle>
-                <CardDescription>
-                    Login to proceed
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Form {...form}>
-                    <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
-                    <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="example@xyz.com" type="email" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your email address for signing in .
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <PasswordInput placeholder="........"  {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">
-            LOGIN
-        </Button>
+    });
 
-                    </form>
-                </Form>
-            </CardContent>
-            <CardFooter className="justify-between">
-                <small>Don't have an account?</small>
-                <Button asChild variant="outline" size="sm">
-                    <Link href="/sign-up">
-                        Sign up
-                    </Link>
-                </Button>
-            </CardFooter>
-        </Card>
+    const handleSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log("kkk");
+        router.push("/dashboard");
+    };
+
+    return (
+        <>
+            <PersonStandingIcon size={50} />
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Login</CardTitle>
+                    <CardDescription>Login to proceed</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="example@xyz.com" type="email" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            This is your email address for signing in.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <PasswordInput placeholder="........" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">LOGIN</Button>
+                        </form>
+                    </Form>
+                </CardContent>
+                <CardFooter className="justify-between">
+                    <small>Don&apos;t have an account?</small>
+                    <Button asChild variant="outline" size="sm">
+                        <Link href="/sign-up">Sign up</Link>
+                    </Button>
+                </CardFooter>
+            </Card>
         </>
-
-    )
-
+    );
 }
--- app/dashboard/employees/columns.tsx
+++ app/dashboard/employees/columns.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { ColumnDef } from "@tanstack/react-table"
+import Image from "next/image";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -16,7 +19,21 @@ export type Employee = {
 export const columns: ColumnDef<Employee>[] = [
   {
     accessorKey: "avatar",
-    header: "Status",
+    header: "",
+    cell:({row}) => {
+     const avatar:string = row.getValue("avatar");
+     const firstName:string = row.getValue("firstName");
+     const lastName:string = row.getValue("lastName");
+
+     return <Avatar>
+      {!! avatar &&
+      <Image height={40} width={40} src={avatar} alt={`${firstName} ${lastName} avatar`}/>
+      }
+      <AvatarFallback className="uppercase">
+        {firstName[0]}{lastName[0]}
+      </AvatarFallback>
+     </Avatar>
+    }
   },
   {
     accessorKey: "firstName",
@@ -33,5 +50,11 @@ export const columns: ColumnDef<Employee>[] = [
   {
     accessorKey: "isTeamLeader",
     header: "",
+    cell: ({row}) => {
+      const isTeamLeader:booloean = row.getValue("isTeamLeader");
+      return isTeamLeader ? <Badge variant="success">
+         Team leader
+      </Badge>:null;
+    }
   },
 ]
